8d61cbe314a8f0ec2f46029ce576339585818ba7
lizz medina <lizzmedina15@gmail.com>
2023-10-06 08:09:23 -0500

    subiendo template

---
+++ workshop/src/main/java/com/workShopApi/workshop/controller.java
@@ -0,0 +1,3 @@
+/*
+texto random to test
+* */
---
+++ workshop/src/test/java/com/workShopApi/workshop/FilesReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workShopApi.workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilesReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
