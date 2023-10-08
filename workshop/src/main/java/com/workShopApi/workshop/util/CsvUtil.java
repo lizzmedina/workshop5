d0539837e791dcb33b4557872d2a0931b65a0d06
Sof-Saos <sofisalamanca93@gmail.com>
2023-10-08 14:38:00 -0500

    metodos de lectura

--- workshop/src/main/java/com/workShopApi/workshop/util/CsvUtil.java
+++ workshop/src/main/java/com/workShopApi/workshop/util/CsvUtil.java
@@ -1,11 +1,40 @@
 package com.workShopApi.workshop.util;
-
-
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
 import org.springframework.stereotype.Component;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class CsvUtil {
+    public List<Persona> LeerCSV(String pathfile) {
+        List<Persona> personas = new ArrayList<>();
 
-    // Lógica para procesar archivos CSV
+        try (Reader reader = new FileReader(pathfile);
+             CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT.withFirstRecordAsHeader())) {
+            personas = csvParser.getRecords().stream()
+                    .map(record -> new Persona(
+                            record.get("Index"),
+                            record.get("UserId"),
+                            record.get("FirstName"),
+                            record.get("LastName"),
+                            record.get("Sex"),
+                            record.get("Email"),
+                            record.get("Phone"),
+                            record.get("Date of birth"),
+                            record.get("Job Title")))
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        personas.forEach(System.out::println);
+        return personas;
+    }
 }
 
