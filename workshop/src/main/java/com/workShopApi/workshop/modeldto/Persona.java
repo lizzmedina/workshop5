5cd4ffdc94b470a6d03c7b10540db9b85ed65632
Santiago Avendaño <118224188+SantiagoGrade@users.noreply.github.com>
2023-10-10 17:05:27 -0500

    Update Persona.java
    Se agregan getter y setter para correcto funcionamiento del CSVValidatorService

--- workshop/src/main/java/com/workShopApi/workshop/modeldto/Persona.java
+++ workshop/src/main/java/com/workShopApi/workshop/modeldto/Persona.java
@@ -1,5 +1,9 @@
+
 package com.workShopApi.workshop.modeldto;
 
+import lombok.Data;
+
+@Data
 public class Persona {
         private String index;
         private String userId;
@@ -23,4 +27,76 @@ public class Persona {
             this.dateOfBirth = dateOfBirth;
             this.jobTitle = jobTitle;
         }
+
+    public String getIndex() {
+        return index;
+    }
+
+    public void setIndex(String index) {
+        this.index = index;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
 }
