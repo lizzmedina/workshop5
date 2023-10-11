2737a4388cf92b1b9e5dcecd8774a6570d5f0351
Sof-Saos <sofisalamanca93@gmail.com>
2023-10-10 19:38:09 -0500

    ignore this

---
+++ validationService/validationService/src/main/java/com/validationService/validationService/service/ValidationCSVService.java
@@ -0,0 +1,38 @@
+package com.validationService.validationService.service;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ValidationService {
+    public void  validateCvs(Persona persona) {
+        public boolean validateLine(String line) {
+            String[] parts = line.split(",");
+            if (parts.length != 3) {
+                return false;
+            }
+
+            String email = parts[0];
+            String dob = parts[1];
+            String jobTitle = parts[2];
+
+            // Reglas de validación para email
+            if (!email.matches("[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}")) {
+                return false;
+            }
+
+            // Reglas de validación para Date of Birth
+            LocalDate dobDate = LocalDate.parse(dob);
+            LocalDate minValidDate = LocalDate.of(1980, 1, 1);
+            if (dobDate.isBefore(minValidDate)) {
+                return false;
+            }
+
+            // Reglas de validación para Job Title
+            List<String> validJobTitles = Arrays.asList("Haematologist", "Phytotherapist", "Building surveyor", "Insurance account manager", "Educational psychologist");
+            if (!validJobTitles.contains(jobTitle)) {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
--- workshop/src/main/java/com/workShopApi/workshop/service/FileProcessingService.java
+++ workshop/src/main/java/com/workShopApi/workshop/service/FileProcessingService.java
@@ -1,10 +1,11 @@
 package com.workShopApi.workshop.service;
-import com.workShopApi.workshop.modeldto.Persona;
-import com.workShopApi.workshop.util.CsvUtil;
+
+
+
+import com.workShopApi.workshop.modeldto.ValidationResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import java.io.IOException;
-import java.util.List;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class FileProcessingService {
@@ -12,23 +13,12 @@ public class FileProcessingService {
     @Autowired
     private ValidationService validationService;
 
-    public String processFile(String file) {
-        int invalidCount = 0, validCount = 0;
-        try {
-            List<Persona> listaPersonas = CsvUtil.leerCSV(file);
-            for (Persona persona : listaPersonas) {
-                //metodo que se supone debe validar | Da error pq no hemos hecho conexion ni nda
-                boolean lineaEsValida = this.validationService.validate(persona, "csv");
-                if (lineaEsValida) {
-                    validCount++;
-                } else {
-                    invalidCount++;
-                }
-            }
-        } catch (IOException e) {
-            return "Error processing the file: " + e.getMessage();
-        }
-        return "Lineas validas: " + validCount + " Lineas Invalidas: " + invalidCount;
+    public ValidationResult processFile(MultipartFile file) {
+        // Lógica para procesar el archivo
+        // Leer línea por línea
+        // Enviar peticiones HTTP al servicio de validación con cada línea
+        // Registrar el número de líneas válidas e inválidas
+        return validationResult;
     }
 }
 
